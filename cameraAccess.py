f074c19bfd707c7b3aa0a9e4bda2607b68c0c310
Luchintha <luchinthaa@gmail.com>
2024-06-06 00:00:56 +0530

    Initial

--- cameraAccess.py
+++ cameraAccess.py
@@ -1,10 +1,18 @@
 import streamlit as st
 from PIL import Image
 
+
+def convert_grey(imgName):
+    img = Image.open(imgName)
+    gry_img = img.convert("L")
+    st.image(gry_img)
+
+uploaded_img = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     normal_img = st.camera_input("Camera")
 
 if normal_img:
-    img = Image.open(normal_img)
-    gry_img = img.convert("L")
-    st.image(gry_img)
+    convert_grey(normal_img)
+elif uploaded_img:
+    convert_grey(uploaded_img)
